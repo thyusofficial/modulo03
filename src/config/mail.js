c61fa80dc7f5f152a22d44e03788a492833b024a
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2019-10-31 12:16:08 -0300

    Subindo

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'eaafd94dcb2a78',
+    pass: 'b0bd6c11b98b87',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
