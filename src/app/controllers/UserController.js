eb0c68b9797dbb9ba1373c4dabc428e6118545d5
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2020-02-02 11:06:54 -0300

    API ajusts before frontend start

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -21,12 +22,21 @@ class UserController {
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
-    const { id, name, email, provider } = await User.create(req.body);
+    await User.create(req.body);
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 
